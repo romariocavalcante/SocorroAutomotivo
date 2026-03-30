a242ae7b1885270bab3afe95601da3d1de193291
romariocavalcant <romariocavalcant@gmail.com>
2026-03-30 13:57:41 -0300

    first commit

--- api/index.py
+++ api/index.py
@@ -1,4 +1,7 @@
 import os
-from socorroauto.wsgi import application
 
-app = application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socorroautomotivo.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+app = get_wsgi_application()
