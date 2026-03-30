cfeaafda50ea0848f3b0e632a2ad82e3ca56606e
romariocavalcant <romariocavalcant@gmail.com>
2026-03-30 14:03:42 -0300

    remove pyproject to fix vercel

--- socorroauto/wsgi.py
+++ socorroauto/wsgi.py
@@ -12,6 +12,3 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socorroautomotivo.settings')
-
-app = get_wsgi_application()
-application = app
