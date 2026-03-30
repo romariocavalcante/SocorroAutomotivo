90e9e0c66dd4dd9c59c474f6f09b895a70e71136
romariocavalcant <romariocavalcant@gmail.com>
2026-03-30 14:00:16 -0300

    first commit

--- socorroauto/wsgi.py
+++ socorroauto/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socorroauto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socorroautomotivo.settings')
 
 app = get_wsgi_application()
 application = app
